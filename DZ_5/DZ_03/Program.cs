4a5c482b3cc77673e368baa437115454fab685cd
ZeN <zensmi@yandex.ru>
2022-08-20 16:41:56 +0300

    DZ

---
+++ DZ_5/DZ_03/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задайте массив вещественных чисел. 
+// Найдите разницу между максимальным и минимальным элементами массива.
+
+double[] RealNumbers = new double[10];
+  
+  for (int i = 0; i < RealNumbers.Length; i++ )
+  {
+    RealNumbers[i] = new Random().Next(1, 100);
+    Console.Write(RealNumbers[i] + " ");
+  }
+
+double maxNumber = RealNumbers[0];
+double minNumber = RealNumbers[0];
+
+  for (int i = 1; i < RealNumbers.Length; i++)
+  {
+    if (maxNumber < RealNumbers[i])
+    {
+      maxNumber = RealNumbers[i];
+    }
+        if (minNumber > RealNumbers[i])
+    {
+      minNumber = RealNumbers[i];
+    }
+  }
+
+  double decision = maxNumber - minNumber;
+
+  Console.WriteLine($"\nРазница между максимальным ({maxNumber}) " +
+                    $"и минимальным({minNumber}) элементами: {decision}");
